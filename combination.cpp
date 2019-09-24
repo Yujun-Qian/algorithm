3d45f7ebf0f07073c967a0ca46592b7dc28682a7
kinglear <yujun.qian@funplus.com>
2019-09-24 14:04:40 +0800

    use reserver in place of resize (but it takes more time, why?)

--- combination.cpp
+++ combination.cpp
@@ -74,50 +74,48 @@ static int visit = 0;
 // m - start index
 // n - end index
 // k - numbers of selected items
-void combination(int m, int n, int k, vector<vector<int>>& result, int* start, int* end, int maxK, int* last) {
+void combination(int m, int n, int k, vector<vector<int>>& result, int* start, int* end, int maxK) {
     visit++;
     *start = result.size();
     if (k > n - m + 1 || k <= 0) {
         *end = result.size();
-        *last = 0;
         return;
     }
 
     if (k == 1) {
         for (int i = m; i <= n; i++) {
-            result.push_back(vector<int>(maxK));
-            result[result.size() - 1][0] = i;
+            vector<int> temp({i});
+            temp.reserve(maxK);
+            result.push_back(std::move(temp));
         }
 
         *end = result.size();
-        *last = 1;
         return;
     }
 
     if (k == n - m + 1) {
-        vector<int> temp(maxK);
+        vector<int> temp;
+        temp.reserve(maxK);
         for (int i = m; i <= n; i++) {
-            temp[i - m] = i;
+            temp.push_back(i);
         }
 
-        result.push_back(temp);
+        result.push_back(std::move(temp));
         *end = result.size();
-        *last = n - m + 1;
         return;
     }
 
     for (int i = m; i <= n - k + 1; i++) {
         int start;
         int end;
-        int last;
-        combination(i + 1, n, k - 1, result, &start, &end, maxK, &last);
+
+        combination(i + 1, n, k - 1, result, &start, &end, maxK);
         for (int j = start; j < end; j++) {
-            result[j][last] = i;
+            result[j].push_back(i);
         }
     }
 
     *end = result.size();
-    *last = k;
     return;
 }
 
@@ -149,7 +147,6 @@ int main(int argc, char** argv) {
         vector<vector<int>> a;
         int start = 0;
         int end = 0;
-        int last = 0;
 
         int product = 1;
         int tempK = k;
@@ -162,7 +159,7 @@ int main(int argc, char** argv) {
         //cout << "product is: " <<  product << endl;
         a.reserve(product);
 
-        combination(0, n - 1, k, a, &start, &end, k, &last);
+        combination(0, n - 1, k, a, &start, &end, k);
         printCombination(a);
 
         if (!debug) {
